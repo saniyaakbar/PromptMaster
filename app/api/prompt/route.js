f4f8a22d2409b2a8c0c1c1aebd5f1a6698739596
Saniya Akbar <saniyaakbar951@gmail.com>
2023-06-18 15:12:42 +0530

    added console log

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,7 +8,7 @@ export const GET = async (req) => {
         await connectToDB();
 
         const prompts = await Prompt.find({}).populate("creator");
-
+            console.log(prompts)
         return new Response(JSON.stringify(prompts),  {status:200})
         
     } catch (error) {
